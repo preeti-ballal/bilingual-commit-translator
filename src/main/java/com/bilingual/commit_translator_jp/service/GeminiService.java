c78018c32af96d9bcddfb6d2b9df7732b1a95e8a
Preeti Ballal <preetiballal.29@gmail.com>
2026-04-20 19:59:58 +0900

    secure API in env file

--- src/main/java/com/bilingual/commit_translator_jp/service/GeminiService.java
+++ src/main/java/com/bilingual/commit_translator_jp/service/GeminiService.java
@@ -1,14 +1,20 @@
 package com.bilingual.commit_translator_jp.service;
 
 import com.google.genai.Client;
+import com.google.genai.types.Content;
+import com.google.genai.types.GenerateContentConfig;
+import com.google.genai.types.Part;
 import com.google.genai.types.GenerateContentResponse;
+
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class GeminiService {
 
-    @Value("${GEMINI_API_KEY}")
+    @Value("${api.gemini.key}")
     private String apiKey;
 
     public String getJapaneseSummary(String englishText) {
@@ -22,7 +28,18 @@ public class GeminiService {
 
         try {
             // 3. Call Gemini 3 Flash
-            GenerateContentResponse response = client.models.generateContent("gemini-3-flash-preview", prompt, null);
+
+            Content content = Content.builder()
+                                .parts(Collections.singletonList(Part.builder()
+                                    .text("You are an expert software engineer. Translate GitHub commit messages into professional business Japanese (Keigo). Use bullet points.")
+                                    .build()))
+                                .build();
+
+            GenerateContentConfig config = GenerateContentConfig.builder()
+                .systemInstruction(content)
+                .build();
+
+            GenerateContentResponse response = client.models.generateContent("gemini-3-flash-preview", prompt, config);
             
             // 4. Return just the text
             return response.text();
